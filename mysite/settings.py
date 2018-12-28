cf8876bfcdba86766ee3c5ac2eca6ed219d3fddc
Ignacio Donoso <i.donosof@alumnos.duoc.cl>
2018-12-28 15:56:06 -0300

    Cambios en settings.py again

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v-8nycnx!3_5tnt9_jcp6fdb_7yihnbo&=zneedm@0sfatt@c='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ' . pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'daka.pythonanywhere.com']
 
 
 # Application definition
